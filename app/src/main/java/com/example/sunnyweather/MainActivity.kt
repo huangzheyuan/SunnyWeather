3be0e99a1f88abb5278ab1c85a9ef43221a0aa3b
huangzheyuan <1710186117@qq.com>
2023-04-24 21:08:44 +0800

    实现搜索全球城市数据功能

--- app/src/main/java/com/example/sunnyweather/MainActivity.kt
+++ app/src/main/java/com/example/sunnyweather/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.sunnyweather
 
-import android.support.v7.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sunnyweather.logic.network.SunnyWeatherNetwork
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
